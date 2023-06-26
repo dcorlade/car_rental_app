e81e88db725386b1d93fea7f52c10e219d8c1a64
DenisCorlade <corladedenis19@gmail.com>
2023-06-26 19:16:23 +0300

    Add layout (Navigation bar and Footer)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
-        <h1 className="hero__title">Find, book, or rent a car!</h1>
+        <h1 className="hero__title">Find and rent a car! </h1>
         <p className="hero__subtitle">
           Streamline your car rental experience with our offertless booking
           process.
@@ -24,8 +24,8 @@ const Hero = () => {
       <div className="hero__image-container">
         <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
-          <div className="hero__image-overlay" />
         </div>
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
